eacaafbd191480ea9675884de45d6fd536ce6d11
Kyle Haver <kyle.haver@slalom.com>
2015-08-28 15:53:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ChatBot
+
+Cleverbot in your very own chatroom
+
+## Testing
+
+To test this script interactively, run:
+
+```
+npm run shell
+```
+# CleverSkibot
---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+// Description:
+//   Cleverbot in your very own chatroom
+//
+// Dependencies:
+//   "cleverbot-node": "^0.2.1"
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   hubot <statement> - <Gets cleverbot's reply>
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   <github username of the original script author>
+module.exports = function (robot) {
+  robot.respond(/(.*)/i, function (res) {
+    var Cleverbot = require('cleverbot-node');
+    cleverbot = new Cleverbot;
+    Cleverbot.prepare(function(){
+      cleverbot.write(res.match[1], function (response) {
+           res.send(response.message);
+      });
+    });
+  });
+};
